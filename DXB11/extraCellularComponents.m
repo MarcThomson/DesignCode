1f38bf3e56347a2655b497e3939204b94240309b
MarcThomson <marc.thomson@colorado.edu>
2018-02-02 12:38:48 -0700

    K1_addeD

---
+++ DXB11/extraCellularComponents.m
@@ -0,0 +1,48 @@
+extraCellular = [...
+0 % c_ACCOA_0 mmol/(10^6 cell)   
+0 % c_ADP_0   mmol/(10^6 cell) % typo assumed  
+0 % c_AKG_0   mmol/(10^6 cell) % typo assumed
+1 % c_ALA_0   mM
+0 % c_AMP_0   mmol/(10^6 cell) 
+1 % c_ARG_0   mM
+1 % c_ASN_0   mM
+1 % c_ASP_0   mM
+0 % c_ATP_0   mmol/(10^6 cell) 
+0 % c_CIT_0   mmol/(10^6 cell) 
+1 % c_CYS_0   mM
+1 % c_EGLC_0  mM
+1 % c_EGLN_0  mM
+1 % c_EGLU_0  mM
+0 % c_F6P_0   mmol/(10^6 cell) 
+0 % c_G6P_0   mmol/(10^6 cell) 
+0 % c_GAP_0   mmol/(10^6 cell) 
+0 % c_GLU_0   mmol/(10^6 cell) 
+1 % c_GLY_0   mM
+1 % % c_HIS_0 mM
+1 % c_ILE_0   mM
+1 % c_LAC_0   mM
+1 % c_LEU_0   mM
+1 % c_LYS_0   mM
+1 % c_mAb_0   mg.L-1
+0 % c_MAL_0   mmol/(10^6 cell) 
+1 % c_MET_0   mM
+0 % c_NAD_0   mmol/(10^6 cell) 
+0 % c_NADH_0  mmol/(10^6 cell) 
+0 % c_NADP_0  mmol/(10^6 cell) 
+0 % c_NADPH_0 mmol/(10^6 cell) 
+1 % c_NH4_0   mM
+0 % c_O2      mmol/(10^6 cell) 
+0 % c_OXA_0   mmol/(10^6 cell) 
+0 % c_PEP_0   mmol/(10^6 cell) 
+1 % c_PHE_0   mM
+1 % c_PRO_0   mM
+0 % c_PYR_0   mmol/(10^6 cell) 
+0 % c_R5P_0   mmol/(10^6 cell) 
+1 % c_SER_0   mM
+0 % c_SUC_0   mmol/(10^6 cell) 
+1 % c_THR_0   mM
+1 % c_TYR_0   mM
+1 % c_VAL_0   mM
+0 % c_X5P_0   mmol/(10^6 cell) 
+1 % c_Cell_0  10^6 cells/mL
+];
