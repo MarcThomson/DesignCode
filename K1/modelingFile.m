1d85d196023c213537359e825d514dd1f77fb682
Eddie796 <31109472+Eddie796@users.noreply.github.com>
2018-02-03 17:41:53 -0700

    Update modelingFile.m

--- K1/modelingFile.m
+++ K1/modelingFile.m
@@ -1,9 +1,10 @@
 %Modeling File
 %% input
-clear all;close all;
+%clear all;close all;
 cellType = 1;
 dt = 0.25; %days;
-tend = 5; %days;
+tend = 10; %days;
+shiftDay=3; %days
 %%
 %load variables
 parameterDefinitions;
@@ -13,47 +14,66 @@ internalLogicals;
 load('stoichMatrix.mat')
 
 t = 0:dt:tend;
-VCD = @(t1)(1-1/(1+exp(-0.5*(t1-9))));
+VCD = @(t1)(1-1./(1+exp(-0.5*(t1-9))));
 
 %tol = 1e-15;
 %options = optimoptions('fmincon','ConstraintTolerance',tol,'StepTolerance',tol,'OptimalityTolerance',tol);
-options = optimoptions('fmincon','MaxFunctionEvaluations',30000);
 
-v = zeros(length(stoichMatrix(1,:)),length(t));
-C = zeros(length(stoichMatrix(:,1))+2,length(t)); %includes R and shift
+steps = length(t)-1;
+numRxns=length(stoichMatrix(1,:));
+numComponents = length(stoichMatrix(:,1));
+
+v = zeros(numRxns,length(t)-1);
+C = zeros(numComponents+2,length(t)); % Adds R and shift
 C(:,1) = initialConditions_vec;
+maxIndex=zeros(12,1);
+objectives=zeros(steps,1);
+
  for i=1:length(t)-1
      if i==1
          y = [initialConditions_vec; cellType];
      else
-         y = [C(:,i-1); cellType];
+         y = [C(:,i); cellType];
      end
 
      inputs = mat2cell(y,ones(1,length(y)),1);
-     rates = rateLaws(inputs {:});
+     rates = rateLaws(inputs{:});
      x0 = rates;
 
-     %linear ops
-     I = find(rates~=0);
+% Nonlinear Optimization Initialization
+     I = find(rates);
      Aeq = stoichMatrix;
-     beq = zeros(length(Aeq(:,1)),1);
-     I_internal=find(internal);
-     for j=1:length(I_internal)
-         Aeq(I_internal(j),:) = 0;
+     beq = zeros(numComponents,1);
+     I_external=find(1-internal);       % We are only assuming pseudosteady-state for intracellular components.
+
+     for j=1:length(I_external)
+         Aeq(I_external(j),:) = 0;
      end
 
      A = zeros(length(x0),length(x0));
      for j=1:length(x0)
-         A(j,i) = reversible(j)-1;
+         A(j,j) = reversible(j)-1;
      end
+     
      b = reversible; 
-
+     
+% Nonlinear Optimization Execution
      f_obj=@(x) sum( ((x(I)-x0(I))./(x0(I))).^2);
-     v(:,i+1) = fmincon(f_obj,x0,A,b,Aeq,beq,[],[],[],options);
-     R = (2*v(1,i+1)+0.64*v(16,i+1))/v(34,i+1);
-
-     %update new values
-     C(1:end-2,i+1) =  C(1:end-2,i) +  (stoichMatrix*v(:,i+1))*dt* VCD(t(i))/1000;
-     C(end-1,i+1) = 0;
+     options = optimoptions('fmincon','MaxFunctionEvaluations',30000);
+     v(:,i) = fmincon(f_obj,x0,A,b,Aeq,beq,[],[],[],options); 
+     R = (2*v(1,i)+0.64*v(16,i))/v(34,i);
+     %objectives(i)=f_obj(v(:,i));
+     f_obj_2=@(x)max( ((x(I)-x0(I))./(x0(I))).^2);
+     [objectives(i),maxIndex(i)]=f_obj_2(v(:,i));
+% Update Solution Matrix
+     C(1:end-2,i+1) =  C(1:end-2,i) +  (stoichMatrix*v(:,i))*dt* VCD(t(i))/2.3*C(11,i)/1000; % Updating cell density
+     C(34,i+1) = C(34,i);
+     C(end-1,i+1) = C(end-1,i);
      C(end, i+1) = R;   
- end
+     
+% Temperature Shift
+if t(i) > shiftDay
+    C(end-1,i+1)=1;    
+end
+     
+end
