c09dca7cf9ae2aa92e3b08839d1d1286c59230a3
Eddie796 <31109472+Eddie796@users.noreply.github.com>
2018-02-12 21:17:22 -0700

    Update modelingFile.m

--- K1/modelingFile.m
+++ K1/modelingFile.m
@@ -1,11 +1,11 @@
 % Modeling File
 
 %% Inputs %%
-clear all;close all;
-cellType = 1;   % Type 1=A, 0=B
-dt = 0.25;      % days
-tend = 30;      % days
-shiftDay=20;     % days
+close all;
+cellType = 1;                                                              % Cells of Type 1=A and 0=B can be selected.
+dt = 0.25;                                                                 % days. Timestep for iteration.
+tend = 30;                                                                 % days. Total days for calculation.
+shiftDay=4;                                                                % days. The day of the temperature shift where the temperature is slightly raised.
 
 %% Load variables from definitions
 parameterDefinitions;
@@ -16,126 +16,83 @@ load('stoichMatrix.mat')
 
 %% Optimization Settings
 
-t = 0:dt:tend;
-VCD = @(t1)(1-1./(1+exp(-0.5*(t1-9))));
+t = 0:dt:tend;                                                             % Defining the time interval for iteration
+VCD = @(t1)(1-1./(1+exp(-0.5*(t1-9))));                                    % Defining the fractional viable cell density
+tol = 1e-10;                                                               % Tolerance for nonlinear optimization
 
-%tol = 1e-10;
-%options = optimoptions('fmincon','ConstraintTolerance',tol,'StepTolerance',tol,'OptimalityTolerance',tol);
-tol = 1e-10;
-options = optimoptions('fmincon','ConstraintTolerance',tol,'OptimalityTolerance',tol,'MaxFunctionEvaluations',30000,'Algorithm','sqp','StepTolerance',1e-15);
-%,'display','off'
+steps = length(t)-1;                                                       % Total number of iterative steps.
+numRxns=length(stoichMatrix(1,:));                                         % Total number of reactions.
+numComponents = length(stoichMatrix(:,1));                                 % Total number of components.
 
-steps = length(t)-1;
-numRxns=length(stoichMatrix(1,:));
-numComponents = length(stoichMatrix(:,1));
-
-v = zeros(numRxns,length(t)-1);
-C = zeros(numComponents+2,length(t)); % Adds R and shift
-C(:,1) = initialConditions_vec;
-maxIndex=zeros(12,1);
-objectives=zeros(steps,1);
-exitFlagVec=zeros(steps,1);
-%%
-
+v = zeros(numRxns,length(t)-1);                                            % Initializes the rates of each reaction for each timestep.
+C = zeros(numComponents+2,length(t));                                      % Adds R and shift
+C(:,1) = initialConditions_vec;                                            % Adds initial conditions to the first column of the solution matrix.
+objectives=zeros(steps,1);                                                 % Initializes objectives. These are minimized in the nonlinear optimization.
+exitFlagVec=zeros(steps,1);                                                % Initialilzes exitflags. These signify if the optimization ran into an error.
+%% Begin Iteration
 for i=1:length(t)-1
-    if i==1
-        y = [initialConditions_vec; cellType];
+    
+    
+    if i==1                                                                % Load inputs
+        y = [initialConditions_vec; cellType];                             % If it is the first step, inputs are loaded from the initial conditions and respective celltype.
     else
-        y = [C(:,i); cellType];
+        y = [C(:,i); cellType];                                            % If it is any other step, inputs are loaded from the previous step.
     end
     
-    inputs = mat2cell(y,ones(1,length(y)),1);
-    rates = rateLaws(inputs{:});
-    x0 = rates;
-    %% Nonlinear Optimization Initialization
+    inputs = num2cell(y,length(y));                                        % Converts inputs to a usable format.
+    rates = rateLaws(inputs{:});                                           % Calculates rates for the input conditions.
+    x0 = rates;                                                            %
     
+    %% Nonlinear Optimization Initializations
     
+    A = zeros(length(x0),length(x0));
+    for j=flip(1:length(x0))
+        if reversible(j)==1
+            A=[A(1:j-1,:);A(j+1:end,:)];
+        else
+            A(j,j) = -1;
+        end
+    end
     
+    Aeq = stoichMatrix;                                                    % Constraint for optimization
+    I_external=find(1-internal);                                           % We are only assuming pseudosteady-state for intracellular components.
+    for j=flip(1:length(I_external))
+        Aeq=[Aeq(1:I_external(j)-1,:);Aeq(I_external(j)+1:end,:)];
+    end
+    
+    H = zeros(length(x0),length(x0));
+    f = zeros(length(x0),1);
     I = find(rates);
-    Aeq = stoichMatrix;
-    I_external=find(1-internal);       % We are only assuming pseudosteady-state for intracellular components.
-    
-    for j=flip(1:length(I_external))
-        %Aeq(I_external(j),:) = 0;
-        Aeq=[Aeq(1:I_external(j)-1,:);Aeq(I_external(j)+1:end,:)];
+    for j = 1:length(I)
+        H(I(j),I(j)) = 2/x0(I(j))^2;
+        f(I(j)) = -2/x0(I(j));
     end
+    
     beq = zeros(length(Aeq(:,1)),1);
-    
-    A = zeros(length(x0),length(x0));
-    for j=flip(1:length(x0))
-        if reversible(j)==1
-            A=[A(1:j-1,:);A(j+1:end,:)];
-        else
-            A(j,j) = -1;
-        end
-    end
-    
     b = zeros(length(A(:,1)),1);
     
-
-I = find(rates);
-    Aeq = stoichMatrix;
-    I_external=find(1-internal);       % We are only assuming pseudosteady-state for intracellular components.
-
-    for j=flip(1:length(I_external))
-        %Aeq(I_external(j),:) = 0;
-        Aeq=[Aeq(1:I_external(j)-1,:);Aeq(I_external(j)+1:end,:)];
-    end
-    beq = zeros(length(Aeq(:,1)),1);
-
-    A = zeros(length(x0),length(x0));
-    for j=flip(1:length(x0))
-        if reversible(j)==1
-            A=[A(1:j-1,:);A(j+1:end,:)];
-        else
-            A(j,j) = -1;
-        end
-    end
-
-    b = zeros(length(A(:,1)),1);
+    %% Nonlinear Optimization Execution
+    options = optimoptions('quadprog','OptimalityTolerance',tol,...        % Define options for quadratic programming
+        'MaxIterations',30000,'Algorithm','interior-point-convex',...
+        'StepTolerance',1e-12,'Display','off');                     
     
+    [v(:,i),objectives(i),exitFlagVec(i)]=...                              % Execute the quadratic optimization
+        quadprog(H,f,A,b,Aeq,beq,[],[],[],options);
     
-    
-    
-    
-    % Nonlinear Optimization Execution
-   
-    
-    
-H = zeros(length(x0),length(x0));
-f = zeros(length(x0),1);    
-for j = 1:length(I)
-       H(I(j),I(j)) = 2/x0(I(j))^2;
-       f(I(j)) = -2/x0(I(j)); 
-end
-options = optimoptions('quadprog','OptimalityTolerance',tol,'MaxIterations',30000,'Algorithm','interior-point-convex','StepTolerance',1e-12,'Display','off');
-[v(:,i), objectives(i), exitFlagVec(i)] =quadprog(H,f,A,b,Aeq,beq,[],[],[],options)    ;
-    
-    %f_obj=@(x) sum( ((x(I)-x0(I))./(x0(I))).^2);
-     
-    
-    %options = optimoptions('fmincon','MaxFunctionEvaluations',30000);
-    % v(:,i) = fmincon(f_obj,x0,A,b,Aeq,beq,[],[],[],options);
-    R = (2*v(1,i)+0.64*v(16,i))/v(34,i);
-     
-    % objectives(i)=f_obj(v(:,i)); %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % f_obj_2=@(x)max( ((x(I)-x0(I))./(x0(I))).^2);
-    % [objectives(i),maxIndex(i)]=f_obj_2(v(:,i));
-    % Update Solution Matrix
-    C(1:end-2,i+1) =  C(1:end-2,i) +  (stoichMatrix*v(:,i))*dt* VCD(t(i))/2.3*C(11,i)/1000; % Updating cell density
-    %C(34,i+1) = C(34,i);
+    R = (2*v(1,i)+0.64*v(16,i))/v(34,i);                                   % Define the R term in terms of current rates
+    C(1:end-2,i+1) =  C(1:end-2,i) +  ...                                  % Updating cell density
+    (stoichMatrix*v(:,i))*dt* VCD(t(i))/2.3*C(11,i)/1000;                  % 
     C(end-1,i+1) = C(end-1,i);
-    C(end, i+1) = R;
+    C(end, i+1) = R;                                                       % Store R in solution matrix
     
     % Temperature Shift
     if t(i) > shiftDay
-        C(end-1,i+1)=1;
+        C(end-1,i+1)=1;                                                    % If the current time is past the designated shift day, shift the temperature.
     end
-    
-    % Feed the bugs!
-    if t(i)-floor(t(i))==0 && C(20,i)<=40
+         
+    if t(i)-floor(t(i))==0 && C(20,i)<=40                                  % If the sugar concentration drops below the threshold of 40, more sugar is added. 
         C(20,i+1)=C(20,i)+3.5;
     end
-    
-    
 end
+plotTool                                                                   % Generates a figure for the species with designated importance.
+
