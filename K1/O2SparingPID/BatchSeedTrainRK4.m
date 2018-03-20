4e5576908feb82f9f74c271326fe65c89e2dd7d7
Edward DeCrescenzo <eddecrescenzo@gmail.com>
2018-03-19 22:45:12 -0600

    Added comments... not complete

--- K1/O2SparingPID/BatchSeedTrainRK4.m
+++ K1/O2SparingPID/BatchSeedTrainRK4.m
@@ -7,89 +7,99 @@ parameters = exp(parameters);
 %parameters  = log(x0);
 
 critDensity = 10;
+system = 1;                         % 1 = perfusion, 0 = batch
 
-system = 1; % 1 = perfusion, 0 = batch
-
-if system == 0
+if system == 0                      % Vessel sizes for the batch seed train
     vesselSize =  [1,4,15,60,250,1000,4E3,20E3,80E3,400E3];
-elseif system ==1
+elseif system ==1                   % Vessel sizes for the perfusion seed train
     vesselSize =  [1,4,15,60,250,1000,4E3,20E3,100E3];
 end
 %% Inputs %%
-tend = 10;
-cellType = 1;
-shift = 0;
-Perfusion = 0;
-shiftDay = 100;
-h = 0.005;
+tend = 10;                          % Defines the length of the seed train
+cellType = 1;                       % Identifies the cell type (1=A)
+shift = 0;                          % Initializes the temperature shift bool
+Perfusion = 0;                      % Identifies the type of reactor (batch)
+shiftDay = 100;                     % Day of the temperature shift
+h = 0.005;                          % Step size for Runge Kutta 4th order in days
 %% Load variables from definitions
-reversibleLogicals;
-internalLogicals;
-initialConditionsSeedTrain;
-initialConditionsCryo;
-load('stoichMatrix.mat')
-t = 0:h:tend;
+reversibleLogicals;                 % Loads logic for reversible/irreversible reactions
+internalLogicals;                   % Loads logic for internal/external components
+initialConditionsSeedTrain;         % Loads initial media conditions
+initialConditionsCryo;              % Loads initial cryovial conditions
+load('stoichMatrix.mat')            % Loads the stoichiometry matrix
+t = 0:h:tend;                       % Defines timescale for seed train
 
-I = find(~internal);
-C0 = initialConditionsCryo_vec(I);
+I = find(~internal);                % Finds external components
+C0 = initialConditionsCryo_vec(I);  % Finds initial conditions for the cryovial     
 
-C_total = [C0];
-t_total = [0];
-R_total = [initialConditions_vec(end)];
-obj_total = [-12];
-exit_total = [1];
-v_total = zeros(34,1);
-VCD_total = [C_total(5)/2.31];
-vesselSize_total = [vesselSize(1)];
-Tend = [];
+C_total = [C0];                             %
+t_total = [0];                              %
+R_total = [initialConditions_vec(end)];     %
+obj_total = [-12];                          %
+exit_total = [1];                           %
+v_total = zeros(34,1);                      %
+VCD_total = [C_total(5)/2.31];              %
+vesselSize_total = [vesselSize(1)];         %
+Tend = [];                                  %
 
-for vessel = 2:length(vesselSize)
+
+% Begins iteration on each vessel size. Each iteration corresponds to a
+% single vessel in the seed train.
+for vessel = 2:length(vesselSize) 
+    % The next line of code calculations the dilution of transitioning from
+    % one vessel to the next. This is done in every step starting from the
+    % cryovial.
     C0 =   (C_total(:,end) * vesselSize(vessel-1) + initialConditions_vec(I) *...
                     (vesselSize(vessel) - vesselSize(vessel-1))) / vesselSize(vessel);
-           
+    %        
     C0(5) = VCD_total(end)*vesselSize(vessel-1)/vesselSize(vessel)*2.31;            
    
-    C = zeros(length(C0),length(t));
-    R = zeros(1,length(t));
-    objVec = zeros(1,length(t));
-    exitVec = zeros(1,length(t));
-    VCD = zeros(1,length(t));
-    v = zeros(34,length(t));  
+    C = zeros(length(C0),length(t)); % Initializes concentration matrix.
+    R = zeros(1,length(t));          % Initializes the redox variable vector
+    objVec = R;                      % Initializes objective function vector 
+    exitVec = R;                     % Initializes the exitflags vector
+    VCD = R;                         % Initializes viable cell density vector
+    v = zeros(34,length(t));         % Initializes the mass flux vector for each component
+    % Initializes the vessel size vector. 
     vesselSize_vec = vesselSize(vessel)*ones(1,length(t));
     
-    C(:,1) = C0;
-    R(1) = R_total(end);
-    exitVec(1) = 1;
-    objVec(1) = -12;
-    VCD(1) = C0(5)/2.31; 
+    C(:,1) = C0;                    % Defines initial media concentrations in the solution matrix
+    R(1) = R_total(end);            % Defines the intial value of the redox variable
+    exitVec(1) = 1;                 % Sets the initial exit flag as success. This is done to help sizing issues.
+    objVec(1) = -12;                % Sets the initial objective to a value. This is done to help sizing issues.
+    VCD(1) = C0(5)/2.31;            % Sets the intial viable cell density (cells/mL)
     
-    i = 1;
-    while VCD(i) < 10
+    
+    %% Runge Kutta (4th Order)
+    i = 1;                          % Sets iterant for following while loop
+    while VCD(i) < 10               % While the VCD is below the transfer value of 10, growth is allowed to proceed.
+    % The line below performs the first step of Runge Kutta (4th order).
+    % This calls the function instantRatesV2, which calculates reaction rates
+    % at the current timestep.
         [K1, R1,~,~,~] =  instantRatesV2(C(:,i),t(i),Perfusion,cellType, shift, R(i), parameters,shiftDay);
         K1 = K1*h;
-
+    % Second step of Runge Kutta
         [K2, R2,~,~,~] = instantRatesV2(C(:,i)+K1/2, t(i) + h/2 ,Perfusion,cellType, shift, R1, parameters,shiftDay);
         K2 = K2*h;
-
+    % Third step of Runge Kutta
         [K3, R3,~,~,~] = instantRatesV2(C(:,i)+K2/2, t(i) + h/2 ,Perfusion,cellType, shift, R2, parameters,shiftDay);
         K3 = K3*h;
-
+    % Fourth step of Runge Kutta
         [K4, R4,~,~,~] = instantRatesV2(C(:,i)+K3, t(i) + h ,Perfusion,cellType, shift, R3, parameters, shiftDay);
         K4 = K4*h;
-
+    % The next line calculates the final reaction rate for each component
         rates = (K1+2*K2+2*K3+K4)/6;
-        Rtest = (R1+2*R2+2*R3+R4)/6;
-        C(:,i+1) = C(:,i) + rates;
-
-
-        C(:,i+1) = C(:,i) + rates;
+        %Rtest = (R1+2*R2+2*R3+R4)/6;
+    % The next line calculates the concentration at the next step
+        C(:,i+1) = C(:,i) + rates; 
+    
    [~, R(i+1),v(:,i+1),objVec(i+1),exitVec(i+1)] =...
        instantRatesV2(C(:,i+1), t(i) + h ,Perfusion,cellType, shift, Rtest, parameters, shiftDay);
 
-
+    %
         VCD(i+1) = C(5,i+1)/2.31 * (1-0.5259/(1+353.3*exp(-0.9381*t(i+1))));
 
-        i = i + 1;
+        i = i + 1;                  % Itera
     end
 C_total = [C_total,C(:,2:i)];
 t_total = [t_total, t_total(end) + t(2:i)];
@@ -103,14 +113,15 @@ Tend = [Tend;t(i)];
 
 
 if vessel>=8 
+    % Identifies the reactor type for saving the file properly.
     if system ==0
         systemString = 'Batch';
     elseif system ==1
         systemString = 'Perfusion';
     end
-    
+    %
     Volume = num2str(vesselSize(vessel)/1000);
-    fileName = [systemString,Volume,'.mat'];
+    fileName = [systemString,Volume,'.mat'];  
     Dt = (4*vesselSize(vessel)/(3*pi))^(1/3)/100; %m
     A = pi/4*Dt^2;
     C_O2_vec = smooth(C(14,2:i),15);
@@ -120,11 +131,11 @@ if vessel>=8
     save(fileName,'t','shiftDay','C_CO2_vec','C_O2_vec','Dt','A');
     t = t2;
 end   
-
 end
 
 
-
+% The next 7 lines define variables in terms of variables found in
+% seedTrainAnalyzer and plotToolV2. 
 C = C_total;
 t = t_total;
 R = R_total;
@@ -134,4 +145,4 @@ v = t_total;
 VCD = VCD_total;
 
 seedTrainAnalyzer
-%plotToolV2
+plotToolV2
